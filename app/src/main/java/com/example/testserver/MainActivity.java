a7d86e89462a497eb5bca67baa70da6a16994fd4
cuikangyuan <cuikangyuan@jd.com>
2021-09-16 15:40:17 +0800

    add test code

--- app/src/main/java/com/example/testserver/MainActivity.java
+++ app/src/main/java/com/example/testserver/MainActivity.java
@@ -1,15 +1,52 @@
 package com.example.testserver;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button start;
+    Button stop;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        start = findViewById(R.id.start_service);
+        stop = findViewById(R.id.stop_service);
+
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                start();
+            }
+        });
+
+        stop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stop();
+            }
+        });
+
+    }
+
+
+    private void start() {
+        Intent intent = new Intent();
+        intent.setClass(this, RemoteService.class);
+        startService(intent);
+    }
+
+    private void stop() {
+        Intent intent = new Intent();
+        intent.setClass(this, RemoteService.class);
+        stopService(intent);
     }
 }
